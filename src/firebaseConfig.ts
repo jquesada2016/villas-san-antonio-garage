950eb7cfdcb8c64638492628a795161d256766d1
Jose Quesada <jquesada2016@fau.edu>
2023-04-09 11:34:49 -0500

    initial implementation of the web app

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmF0vvR2v5oGZCwGJDD6n9cCRmxucTzj8",
+  authDomain: "villas-san-antonio-iot-garage.firebaseapp.com",
+  projectId: "villas-san-antonio-iot-garage",
+  storageBucket: "villas-san-antonio-iot-garage.appspot.com",
+  messagingSenderId: "835924723049",
+  appId: "1:835924723049:web:4d636b18d0471f3c4c69f8",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html", "src/**/*.ts", "wasm/**/*.rs"],
+  content: [
+    "index.html",
+    "src/**/*.ts",
+    "wasm/**/*.rs",
+    "wasm/daisyui_component_classes.txt",
+  ],
   theme: {
     extend: {},
   },
